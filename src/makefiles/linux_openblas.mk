d3e26a40e95469c8821e6799ab716d4922119658
Ondrej Platek <ondrej.platek@seznam.cz>
2013-06-18 05:17:15 -0400

    Added option --static-fst to and adde fPIC to Linux Makefiles

--- src/makefiles/linux_openblas.mk
+++ src/makefiles/linux_openblas.mk
@@ -6,12 +6,12 @@ CXXFLAGS = -msse -Wall -I.. \
       -Wno-sign-compare \
       -DHAVE_EXECINFO_H=1 -rdynamic -DHAVE_CXXABI_H \
       -DUSE_KALDI_SVD -DHAVE_OPENBLAS -I $(OPENBLASROOT)/include \
-      -I ../../tools/openfst/include \
+      -I $(FSTROOT)/include \
       $(EXTRA_CXXFLAGS) \
       -g # -O0 -DKALDI_PARANOID 
 
 LDFLAGS = -rdynamic
-LDLIBS = $(EXTRA_LDLIBS) -ldl $(OPENFSTLIBS) $(OPENBLASLIBS) -lm -lpthread
+LDLIBS = $(EXTRA_LDLIBS) $(OPENFSTLIBS) $(OPENBLASLIBS) -lm -lpthread -ldl 
 CC = g++
 CXX = g++
 AR = ar
