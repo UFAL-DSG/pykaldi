a1057f8ca9921c7281a9b0de79b603a5f8919d50
oplatek <oplatek@5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8>
2013-06-25 16:02:19 +0000

    Refactoring
    git-svn-id: svn+ssh://svn.code.sf.net/p/kaldi/code/sandbox/oplatek/@2572 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- src/python-kaldi-decoding/compute-wer-test.c
+++ src/python-kaldi-decoding/compute-wer-test.c
@@ -1,4 +1,4 @@
-#include "test_cffi_python_dyn.h"
+#include "test-cffi-python-dyn.h"
 
 int main(int argc, char **argv) {
     return testSharedLib("libkaldi-cffi.so", "compute_wer_like_main", argc, argv);
--- src/python-kaldi-decoding/online-wav-gmm-decode-faster-test.c
+++ src/python-kaldi-decoding/online-wav-gmm-decode-faster-test.c
@@ -1,4 +1,4 @@
-#include "test_cffi_python_dyn.h"
+#include "test-cffi-python-dyn.h"
 
 int main(int argc, char **argv) {
     return testSharedLib("libkaldi-cffi.so", "online_wav_gmm_decode_faster_like_main", argc, argv);
