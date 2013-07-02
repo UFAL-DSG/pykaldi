13575eab04b11a2d862f4a79d25d125018ebb15b
Ondrej Platek <ondrej.platek@seznam.cz>
2013-07-02 17:35:55 +0200

    binutils and dummy decoders in pykaldi module test runs

--- src/python-kaldi-decoding/pykaldi/binutils/__init__.py
+++ src/python-kaldi-decoding/pykaldi/binutils/__init__.py
@@ -10,7 +10,7 @@ For running pykaldi you need cffi module installed!
 
 # binary like utils from Kaldi
 
-binffi = FFI()
+ffibin = FFI()
 binheader = '''
 int compute_mfcc_feats_like_main(int argc, char **argv);
 int gmm_latgen_faster_like_main(int argc, char **argv);
@@ -18,10 +18,10 @@ int lattice_best_path_like_main(int argc, char **argv);
 int compute_wer_like_main(int argc, char **argv);
 int online_wav_gmm_decode_faster_like_main(int argc, char *argv[]);
 '''
-binffi.cdef(binheader)
+ffibin.cdef(binheader)
 bin_lib_path = 'libpykaldi.so'
 try:
-    binlib = binffi.dlopen(bin_lib_path)
+    libbin = ffibin.dlopen(bin_lib_path)
 except OSError as e:
     print 'Could not find the C shared library %s' % bin_lib_path
     raise e
--- src/python-kaldi-decoding/pykaldi/decoders/kaldi_decoders_test.py
+++ src/python-kaldi-decoding/pykaldi/decoders/kaldi_decoders_test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from kaldiDecoders import NbListDecoder
+from kaldi_decoders import NbListDecoder
 import unittest
 
 
