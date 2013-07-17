40b85959fadd382d2bd82cb68c4692d4dbaa3ec5
Ondrej Platek <ondrej.platek@seznam.cz>
2013-07-17 19:22:54 +0200

    using tag2_settings, pretrained LM+dict, improved scripts and kaldi src

--- egs/kaldi-vystadial-recipe/s5/conf/train_conf.sh
+++ egs/kaldi-vystadial-recipe/s5/conf/train_conf.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 
-# Vystadial data 1 channel 16000 16-bit
+# Vystadial data: 1 channel, 16000Hz, 16-bit wav files
 export DATA_ROOT="/ha/projects/vystadial/data/asr/en/voip/"
-
-# Test-time language model order
-# The script just copies the arpa LM 
-# export LM_ORDER=3 
-# export ARPA_MODEL="/ha/projects/vystadial/git/alex/resources/lm/caminfo/arpa_trigram"
-
-export LM_ORDER=3
-# Unset or empty ARPA_MODEL variable means 
-# that the script will build the LM itself
-unset ARPA_MODEL 
-
-# export DICTIONARY="/ha/projects/vystadial/git/alex/resources/lm/caminfo/dict"
-
-# Unset or empty DICTIONARY variable means 
-# that the script will build the DICTIONARY itself
-unset DICTIONARY
-
-# Storage dir for MFCC. Need a lot of space. 
-export MFCC_DIR="./mfcc"
 
 # How big portion of available data to use
 # everyN=3    ->   we use one third of data
 everyN=1
+
+
+export LM_ORDER=3 
+
+# Unset or empty ARPA_MODEL variable means 
+# that the script will build the LM itself
+# unset ARPA_MODEL 
+
+# The script just copies the arpa LM 
+# export ARPA_MODEL="/ha/projects/vystadial/git/alex/resources/lm/caminfo/arpa_trigram"
+
+
+# Unset or empty DICTIONARY variable means 
+# that the script will build the DICTIONARY itself
+# unset DICTIONARY
+
+export DICTIONARY="/ha/projects/vystadial/git/alex/resources/lm/caminfo/dict"
+
+
+# Storage dir for MFCC. Need a lot of space. 
+export MFCC_DIR="./mfcc"
+
 
 # Train monophone models on a subset of the data of this size
 monoTrainData=1000
