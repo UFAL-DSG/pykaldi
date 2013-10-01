4d47486b2bf07c4616c1ddf3edfbf3056d9cc0d3
Ondrej Platek <ondrej.platek@seznam.cz>
2013-10-01 11:14:32 +0200

    Added debugging output after mffcc and delta-delta for kaldi bins

--- src/pykaldi/pykaldi/binutils/run_gmm_latgen_faster.sh
+++ src/pykaldi/pykaldi/binutils/run_gmm_latgen_faster.sh
@@ -19,26 +19,26 @@ compute-mfcc-feats  --verbose=2 --config=$mfcc_config scp:$wav_scp \
 # For debugging
 compute-mfcc-feats  --verbose=2 --config=$mfcc_config scp:$wav_scp \
   ark,t,scp:$mfccdir/raw_mfcc.ark.txt,${feat_scp}.txt || exit 1;
-
 # For debugging
 add-deltas "scp,s,cs:$feat_scp" "ark,t:$mfccdir/dd_mfcc.ark.txt"
-#runtime version
-# gmm-latgen-faster --config=$decode_config \
-#     --word-symbol-table=$wst $model $hclg \
-#     "ark,s,cs:copy-feats scp:$feat_scp ark:- | add-deltas  ark:- ark:- |" \
-#     "ark:|gzip - c > $lattice"
 
-# lattice-best-path --lm-scale=15 --word-symbol-table=$wst \
-#     "ark:gunzip -c $lattice|" ark,t:$gmm_latgen_faster_tra
-# 
-# cat $gmm_latgen_faster_tra | ./int2sym.pl -f 2- $wst > $gmm_latgen_faster_tra_txt
-# 
-# # reference is named based on wav_scp 
-# ./build_reference.py $wav_scp $decode_dir  
-# reference=$decode_dir/`basename $wav_scp`.tra
-# compute-wer --text --mode=present ark:$reference ark,p:$gmm_latgen_faster_tra_txt
-# 
-# echo; echo "Reference"; echo
-# cat $reference
-# echo; echo "Decoded"; echo
-# cat $gmm_latgen_faster_tra_txt
+runtime version
+gmm-latgen-faster --config=$decode_config \
+    --word-symbol-table=$wst $model $hclg \
+    "ark,s,cs:copy-feats scp:$feat_scp ark:- | add-deltas  ark:- ark:- |" \
+    "ark:|gzip - c > $lattice"
+
+lattice-best-path --lm-scale=15 --word-symbol-table=$wst \
+    "ark:gunzip -c $lattice|" ark,t:$gmm_latgen_faster_tra
+
+cat $gmm_latgen_faster_tra | ./int2sym.pl -f 2- $wst > $gmm_latgen_faster_tra_txt
+
+# reference is named based on wav_scp 
+./build_reference.py $wav_scp $decode_dir  
+reference=$decode_dir/`basename $wav_scp`.tra
+compute-wer --text --mode=present ark:$reference ark,p:$gmm_latgen_faster_tra_txt
+
+echo; echo "Reference"; echo
+cat $reference
+echo; echo "Decoded"; echo
+cat $gmm_latgen_faster_tra_txt
