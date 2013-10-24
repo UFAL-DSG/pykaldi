3475ca004a4e5e182743bca145c627f5b8ff0319
Ondrej Platek <ondrej.platek@seznam.cz>
2013-10-24 17:34:25 +0200

    minor fixes

--- src/pykaldi/pykaldi/binutils/run_pykaldi-latgen-faster-decoder.sh
+++ src/pykaldi/pykaldi/binutils/run_pykaldi-latgen-faster-decoder.sh
@@ -11,6 +11,8 @@ cat $decode_config $mfcc_config > $tmp_config
 echo 1>&2; echo Using config $tmp_config 1>&2 ; echo 1>&2
 cat $tmp_config 1>&2 ; echo 1>&2
 
+export LD_LIBRARY_PATH=`pwd`/../../../dec-wrap:$LD_LIBRARY_PATH
+
 # cgdb -q -x .gdbinit_latgen --args \
 python pykaldi-latgen-faster-decoder.py $wav_scp $batch_size $pykaldi_latgen_tra \
     --verbose=2 --acoustic-scale=0.1 --config=$tmp_config \
