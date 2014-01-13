9fae5cee8a9018aeca8c059b7e0446cc52df7447
Ondrej Platek <ondrej.platek@seznam.cz>
2014-01-13 17:12:10 +0100

    Enable downloaded LDA models in binutils
    Still decoding noise only

--- src/pykaldi/pykaldi/binutils/path.sh
+++ src/pykaldi/pykaldi/binutils/path.sh
@@ -19,12 +19,21 @@ lattice=$decode_dir/lat.gz
 # common configs
 mfcc_config=conf/mfcc.conf
 decode_config=conf/decode.conf
-model=$exp_dir/final.mdl
-hclg=$exp_dir/HCLG.fst
+
 wst=$exp_dir/words.txt
-# if no LDA matrix specified -> use delta + delta-delta
-# Note that $model has to be trained with LDA enabled if using LDA
-lda_matrix="$exp_dir/final.mat"   # alternatives empty | path to LDA matrix
+
+lda=no
+if [ $lda='yes' ] ; then
+    # Note that $model has to be trained with LDA enabled if using LDA
+    model=$exp_dir/final_lda.mdl
+    hclg=$exp_dir/HCLG_lda.fst
+    lda_matrix="$exp_dir/final_lda.mat"
+else
+    # if no LDA matrix specified -> use delta + delta-delta
+    model=$exp_dir/final.mdl
+    hclg=$exp_dir/HCLG.fst
+    lda_matrix=""
+fi
 
 pykaldi_dir=`pwd`/../..
 export LD_LIBRARY_PATH=$pykaldi_dir:$LD_LIBRARY_PATH
