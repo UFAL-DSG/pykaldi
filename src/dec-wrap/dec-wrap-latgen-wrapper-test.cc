d797e2929f7fc97ef1ad0bcb088d07f8e1d87c4e
Ondrej Platek <ondrej.platek@seznam.cz>
2014-02-27 23:17:49 +0100

    logic in wrapper-test

--- src/dec-wrap/dec-wrap-latgen-wrapper-test.cc
+++ src/dec-wrap/dec-wrap-latgen-wrapper-test.cc
@@ -23,6 +23,7 @@
 
 #include "base/kaldi-math.h"
 #include "feat/wave-reader.h"
+#include "dec-wrap/dec-wrap-latgen-wrapper.h"
 
 using namespace kaldi;
 using std::string;
@@ -43,7 +44,39 @@ void ReadWav(const std::string & wav_path, Vector<BaseFloat> &out_v) {
 int main(int argc, char *argv[])
 {
   std::vector<string> args(argv, argv+argc);
+  if (args.size() < 2) {
+    std::cerr << "Usage: " << args[0] << ": <wave_file> <GmmLatgenOptions>" 
+      << std::endl;
+  }
   Vector<BaseFloat> wave;
   ReadWav(args[1], wave);
+
+  GmmLatgenWrapper rec;
+  rec.Setup(args.size() - 2, argv + 2);
+
+  // TOOD the use case is to switch FrameIn and decode
+  // often so the decoder can decode as the user speaks
+  rec.FrameIn(wave_to_std_vector, len_wave);
+
+  size_t decoded_frames = 0;
+  size_t decoded_now = 0;
+  size_t max_decoded = 10;
+  do {
+    decoded_frames += decoded_now;
+    decode_now = rec.Decode(max_decoded);
+  } while(decoded_now > 0);
+
+  rec.PruneFinal();
+  
+  fst::VectorFst<fst::LogArc> word_post_lat;
+  double tot_lik;
+  rec.GetLattice(&word_post_lat, &tot_lik);
+
+  std::cout << "Likelihood of the utterance is " << tot_lik << std::endl;
+
+  // TODO convert lattice to tropical semiring
+  // TODO SingleShortestPaht http://openfst.cs.nyu.edu/doxygen/fst/html/shortest-path_8h.html
+  // TODO write shortest path labels to output
+
   return 0;
 }
