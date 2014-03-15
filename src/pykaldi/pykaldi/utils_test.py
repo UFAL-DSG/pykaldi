d716ea2969eadd9726b4a5a05dd1bce55fd796cb
Ondrej Platek <ondrej.platek@seznam.cz>
2014-03-15 22:11:41 +0100

    fixed unittest support python2.6

--- src/pykaldi/pykaldi/utils_test.py
+++ src/pykaldi/pykaldi/utils_test.py
@@ -15,7 +15,14 @@
 # limitations under the License. #
 from __future__ import unicode_literals
 
-import unittest
+try:
+    from unittest import skipIf as ut_skipIf, ut_TestCase, main as ut_main
+except ImportError:
+    try:
+        from unittest2 import skipIf as ut_skipIf, ut_TestCase, main as ut_main
+    except ImportError:
+        print(('For Python 2.7+ you need unittest module.'
+               'For Python 2.6 you need unittest 2 module'))
 import copy
 from utils import expand_prefix
 from utils import fst_shortest_path_to_lists
@@ -24,7 +31,7 @@ import os
 from subprocess import call
 
 
-class TestExpandPref(unittest.TestCase):
+class TestExpandPref(ut_TestCase):
 
     def setUp(self):
         self.test = {'x': '1',
@@ -56,8 +63,8 @@ class TestExpandPref(unittest.TestCase):
         self.assertTrue(gold['innerlist'][0] == test['innerlist'][0], 'list expansion fails')
 
 
-@unittest.skipIf(call(['which', 'fstcompile']) != 0, 'We need to compile testing fst using fstcompile')
-class TestLatticeToNbest(unittest.TestCase):
+@ut_skipIf(call(['which', 'fstcompile']) != 0, 'We need to compile testing fst using fstcompile')
+class TestLatticeToNbest(ut_TestCase):
 
     def setUp(self):
         shortest_txt = os.path.join(os.path.dirname(__file__), 'test_shortest.txt')
@@ -79,4 +86,4 @@ class TestLatticeToNbest(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    unittest.main()
+    ut_main()
