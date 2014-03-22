550db02f80088af6e00c30734c8737f3139a62d5
Ondrej Platek <ondrej.platek@seznam.cz>
2014-03-22 20:32:39 +0100

    Added sphinx doc, but not generating anythin useful

---
+++ src/pykaldi/doc/index.rst
@@ -0,0 +1,41 @@
+.. Pykaldi master file
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to pykaldi documentation!
+===========================================================
+
+Pykaldi directory is a Python wrapper around C++ OnlineLatgenRecogniser.
+Currently, only OnlineLatgenRecogniser class from whole Kaldi library is interfaced to Python,
+but probably the support will be growing.
+Note that also OpenFST objects are interfaced through Python.
+
+Contents
+----------------------------------------------------------
+.. toctree::
+   :maxdepth: 1
+
+   _man_rst/README
+   _man_rst/INSTALL
+   _man_rst/index.rst
+
+Pykaldi modules
+------------
+
+.. toctree::
+   :maxdepth: 2
+
+   _rst/pykaldi
+
+Indices and tables
+------------------
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
+How to write documentation
+--------------------------
+
+* `Quick cheatsheet for ReST and Sphinx <http://matplotlib.org/sampledoc/cheatsheet.html>`_
+* `More thorough documentation with code examples <http://packages.python.org/an_example_pypi_project/sphinx.html>`_
---
+++ src/pykaldi/doc/manual_rsts.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This code is PEP8-compliant. See http://www.python.org/dev/peps/pep-0008.
+
+import os.path
+import shutil
+import fnmatch
+
+root = '../kaldi'
+
+
+def get_all_rsts(root='.'):
+    """ Searches for all rst files under the root directory.
+
+    At this moment, there files are not sorted correctly as files from deeper directories can appear
+    before the files fro upper directories.
+    The solution would be to list first the files in a directory and after that files in the subdirectories and so on
+    I will have to find a better way of listing files in subdirectories, may be ``walk``?
+
+    :param root: the root directory form where the search begin
+    :return: the sorted list of names of rst files
+    """
+    f, glob = set([]), '*.rst'
+    for root, dirnames, filenames in os.walk(root, followlinks=False):
+        for filename in fnmatch.filter(filenames, glob):
+            f.add(os.path.join(root, filename))
+    f = list(f)
+    # f.sort()
+
+    for i in f:
+        shutil.copy2(i, os.path.join('_man_rst', i.replace('/', '.').replace('...', '')))
+
+    return f
+
+rsts = [os.path.join('..', f) for f in ['README.rst']]
+rsts.extend(get_all_rsts(root))
+
+with open(os.path.join('_man_rst', 'index.rst'), 'w') as f:
+    f.write('Index of manually written in source code tree documentation\n')
+    f.write('===========================================================\n')
+    f.write('.. toctree::\n')
+    f.write('   :maxdepth: 2\n')
+    f.write('   \n')
+    for i in rsts:
+        if 'kaldi' in i:
+            f.write('   %s\n' % i.replace('/', '.').replace('...', '').replace('.rst', ''))
