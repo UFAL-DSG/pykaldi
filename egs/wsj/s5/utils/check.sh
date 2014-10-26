8e1a4f5eeae66a10bfa50a4fc07ecf83511b4bcb
Ondrej Platek <ondrej.platek@seznam.cz>
2014-10-26 23:19:08 +0100

    check working under linux

--- egs/wsj/s5/utils/check.sh
+++ egs/wsj/s5/utils/check.sh
@@ -4,7 +4,14 @@ mkdir -p ckpts
 
 fullname="./ckpts/`echo $@ | sed 's:[/!]:_:g'`"
 if [[ ${#fullname} -gt 255 ]] ; then
-  hash="`echo $fullname | md5`"
+  if which md5 ; then 
+      hash="`echo $fullname | md5`" 
+  elif which md5sum ; then 
+      hash="`echo $fullname | md5sum | cut -d ' ' -f 1`" ;
+  else
+      hash="BADHASH_`date +%s`"
+  fi
+
   prefix_len=`expr 255 - ${#hash}`
   name="${fullname:0:$prefix_len}$hash"
 else
