a6e549087bd36a397d558d956514d0fc8f1628e3
Ondrej Platek <ondrej.platek@seznam.cz>
2015-04-06 19:50:04 +0200

    fixes pykaldi docker

--- docker/build_master_head.sh
+++ docker/build_master_head.sh
@@ -2,6 +2,5 @@
 repo_root="$(git rev-parse --show-toplevel)"
 
 echo "Note that the context is much bigger than from dockerhub, if you build it locally."
-docker build --pull -f $repo_root/docker/production.dockerfile -t ufaldsg/pykaldi $repo_root
-# # For debugging remove the --rm option
-# docker build --pull --rm -f $repo_root/docker/production.dockerfile -t ufaldsg/pykaldi $repo_root
+# For debugging remove the --rm option
+docker build --pull --rm -f $repo_root/docker/production/Dockerfile -t ufaldsg/pykaldi $repo_root
